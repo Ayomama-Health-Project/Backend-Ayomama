59f204681d9aa802b176bb21799c27de01ddeda7
olathedev <tilewaolatoye17@gmail.com>
2025-10-04 20:25:40 +0100

    feat: removed zod

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -6,13 +6,21 @@ import {
   logoutUser,
 } from "../controllers/authController.js";
 import { protectRoute } from "../middleware/authMiddleware.js";
-import { validateRequest } from "zod-express-middleware";
-import { loginSchema, registerSchema } from "../middleware/validateSchema.js";
+// import { validateRequest } from "zod-express-middleware";
+// import { loginSchema, registerSchema } from "../middleware/validateSchema.js";
 
 const router = express.Router();
 
-router.post("/register", validateRequest({ body: registerSchema }), signUp);
-router.post("/login", validateRequest({ body: loginSchema }), loginUser);
+router.post(
+  "/register",
+  // validateRequest({ body: registerSchema }),
+  signUp
+);
+router.post(
+  "/login",
+  // validateRequest({ body: loginSchema }),
+  loginUser
+);
 // router.get("/me", protectRoute, getProfile);
 router.post("/logout", protectRoute, logoutUser);
 
